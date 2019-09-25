6da9459d8367961bad486e27f19bb558f85c7a68
Matt Shinall <matt.shinall@gmail.com>
2019-09-25 13:42:33 -0400

    update

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
 import time
+import fourletterphat as flp
+
 
 while True:
-	time.sleep(5)
+	flp.print_str(time.strftime("%I%M"))
+	amp = time.strftime("%p")
+	if amp == "AM":
+		flp.set_decimal(1, 0)
+	else:
+		flp.set_decimal(1, 1)
+	flp.show()
+	time.sleep(0.1)
 
 
