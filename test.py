37b460fd5f1d892c7849bac83a9a863d3792e1d7
Matt Shinall <matt.shinall@gmail.com>
2022-09-06 13:31:02 -0400

    update

--- test.py
+++ test.py
@@ -6,10 +6,14 @@ import fourletterphat as flp
 flp.set_brightness(15)
 
 while True:
-    tim = time.strftime("%H%M")
+    tim = time.strftime("%I%M")
     sec = time.strftime("%S")
     flp.print_str(tim)
     amp = time.strftime("%p")
+    if amp == "PM":
+    		flp.set_decimal(3, 1)
+    else:
+    		flp.set_decimal(3, 0)
     if int(sec) % 2 == 0:
         flp.set_decimal(1, 1)
     else:
